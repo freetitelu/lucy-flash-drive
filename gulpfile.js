e5714ab26c441decb923c240035604ae9e8f7029
freetitelu <freetitelu@gmail.com>
2016-10-23 23:12:37 +0300

    remove _0 and add dash-grid with paddings and _0,_5,_10,_15, and nulled pd/mg in insted grids

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,7 @@ var autoprefixer     = require('autoprefixer');
 var vars             = require('gulp-vars');
 
 gulp.task("default", [
-    'build:flashDrive',//1
-    'build:flashDriveNonVar',//2
+    'build:flashDrive',
 ]);
 
 //1
@@ -48,46 +47,5 @@ gulp.task('flashDrive', function() {
     './dist/lucy.grid.css'
     ])
     .pipe(concat('lucy.flashdrive.css'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-
-//2
-gulp.task('build:flashDriveNonVar', function() {
-  runSequence(//'helperNonVar',
-              'gridNonVar',
-              'flashDriveNonVar'
-              );
-});
-/*
-gulp.task('helperNonVar', function() {
-  return gulp.src([
-    './src/lucy.helper.css',
-    ])
-    .pipe(vars())
-    .pipe(postcss([ customProperties()  ]))
-    .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
-    .pipe(concat('lucy.helper.nonvar.css'))
-    .pipe(gulp.dest('./dist/'));
-});
-*/
-gulp.task('gridNonVar', function() {
-  return gulp.src([
-    './src/lucy.grid.nonvar.css',
-    ])
-    .pipe(concat('lucy.grid.css'))
-    .pipe(vars())
-    .pipe(postcss([ customProperties()  ]))
-    .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
-    .pipe(concat('lucy.grid.nonvar.css'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('flashDriveNonVar', function() {
-  return gulp.src([
-    './dist/lucy.helper.css',
-    './dist/lucy.grid.nonvar.css'
-    ])
-    .pipe(concat('lucy.flashdrive.nonvar.css'))
     .pipe(gulp.dest('./dist/'));
 });
